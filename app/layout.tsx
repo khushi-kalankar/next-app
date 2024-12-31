351e50f2d4cd3a7d18f11e29378232f7099e7472
Khushi <khushikalankar@gmail.com>
2024-12-31 18:27:34 +0530

    added navbar in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="p-4 font-semibold text-white bg-black">
+            Next-app website
+        </div>
         {children}
       </body>
     </html>
